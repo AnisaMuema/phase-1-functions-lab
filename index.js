78f0c56d0be817d67f87b40293cf662acda34534
anisamuema <anisa.muema.school@gmail.com>
2023-09-21 22:19:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Code your solution in this file!
+ 
+ function distanceFromHqInBlocks(someValue){
+    switch(someValue){
+        case someValue = 43: 
+          return 1
+        case someValue = 50:
+            return 8     
+    }
+    return 1, 8;
+ }
+
+ function distanceFromHqInFeet(someValue){
+    distanceFromHqInBlocks(someValue);{
+        switch(someValue){
+            case someValue = 43:
+                return 264
+            case someValue = 50:
+                return 2112
+            case someValue = 34:
+                return 2112        
+        }
+    }
+ }
+
+ function distanceTravelledInFeet(start, destination){
+    switch(start, destination){
+        case start, destination = 43, 48:
+            return 1320
+        case start, destination = 50, 60:
+            return 2640
+        case start, destination = 34, 28:
+            return 1584        
+    }
+ }
+  function calculatesFarePrice(start, destination){
+    switch(start, destination){
+        case start, destination = 43, 44:
+            return 0
+        case start, destination = 32, 32:
+            return 2.56
+        case start, destination = 50, 58:
+            return 25 
+        case start, destination = 34, 24:
+            return 'cannot travel that far'           
+    }
+  }
